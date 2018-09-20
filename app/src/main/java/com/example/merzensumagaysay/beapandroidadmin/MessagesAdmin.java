41062b0bbc9cf66c681ba0f4b90f67410747df2b
Merzen Sumagaysay <yourgitemail@example.com>
2018-09-21 00:23:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/merzensumagaysay/beapandroidadmin/MessagesAdmin.java
@@ -0,0 +1,14 @@
+package com.example.merzensumagaysay.beapandroidadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessagesAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages_admin);
+        getSupportActionBar().setTitle("Messages");
+    }
+}
